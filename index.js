bb8c0e06092b2172e4c04fa6378df6d20d12f41d
Faaiz Ahmed <faaizahmad03@icloud.com>
2026-03-15 13:32:58 +0200

    serve frontend correctly

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.use(morgan('tiny'))
 
 app.set('json spaces', 2)
 
+app.use(express.static('dist'))
+
 let persons = [
   { id: '1', name: 'Arto Hellas', number: '040-123456' },
   { id: '2', name: 'Ada Lovelace', number: '39-44-5323523' },
